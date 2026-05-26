331846e25a53e1ceaf878609b74f9ccbf5b84075
lagma <lagmajin@gmail.com>
2026-05-26 09:54:36 +0900

    Make shot tester auto-fire

--- Assets/Scripts/Test/ShotTester.cs
+++ Assets/Scripts/Test/ShotTester.cs
@@ -12,25 +12,45 @@ namespace OpenGS
     [DisallowMultipleComponent]
     public class ShotTester : MonoBehaviour
     {
+        [SerializeField] private bool auto = true;
+        [SerializeField] private float autoInterval = 1.0f;
         [SerializeField][Required]private GameObject bullet;
+
+        private float elapsed;
 
 
         // Start is called before the first frame update
         void Start()
         {
-
+            elapsed = 0f;
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (!auto)
+            {
+                return;
+            }
 
+            elapsed += Time.deltaTime;
+            if (elapsed >= autoInterval)
+            {
+                elapsed = 0f;
+                Test();
+            }
         }
 
         [Button("" )]
         public void Test()
         {
+            if (bullet == null)
+            {
+                Debug.LogWarning("[ShotTester] bullet prefab is not assigned.");
+                return;
+            }
 
+            Instantiate(bullet, transform.position, transform.rotation);
         }
     }
 
