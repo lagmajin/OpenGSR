fff72ce1ed089ec9af0bbe49121885c3fc6a01d0
lagmajin@gmail.com <lagmajin@gmail.com>
2026-05-29 15:15:20 +0900

    refactor: route grenade and hit effects through service

--- Assets/Scripts/Bullet/GrenadeBulletAgent.cs
+++ Assets/Scripts/Bullet/GrenadeBulletAgent.cs
@@ -1,37 +1,40 @@
 using UnityEngine;
-
+using Zenject;
 
 namespace OpenGS
 {
     [DisallowMultipleComponent]
-    public class GrenadeBulletAgent:AbstractBulletAgent
+    public class GrenadeBulletAgent : AbstractBulletAgent
     {
         private Vector2 velocity;
         private float gravity = -9.8f;
-        private float damage=0;
-        [SerializeField]private GameObject explosionPrefab;
+        private float damage = 0;
+        [SerializeField] private GameObject explosionPrefab;
         [SerializeField] private LayerMask layerMask;
+        private IEffectService effectService;
 
-        float Speed = 0;
+        [Inject]
+        private void Construct([InjectOptional] IEffectService effectService)
+        {
+            this.effectService = effectService;
+        }
+
+        private float Speed = 0;
+
         public override void Launch(Vector2 direction, float speed, float damage = 0)
         {
             Speed = speed;
-            this.velocity = direction.normalized * speed;
+            velocity = direction.normalized * speed;
             this.damage = damage;
-
         }
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+
+        private void Start()
         {
-
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
             velocity.y += gravity * Time.deltaTime;
-
-            // à⁄ìÆ
             transform.position += (Vector3)(velocity * Time.deltaTime);
 
             if (velocity != Vector2.zero)
@@ -43,43 +46,37 @@ namespace OpenGS
             OnColision();
         }
 
-
-        void OnColision()
+        private void OnColision()
         {
             Vector2 direction = transform.up;
             var hit = Physics2D.Raycast(transform.position, direction, Speed * Time.deltaTime, layerMask);
 
             if (hit.collider != null)
             {
-                // è’ìÀëŒè€ÇéÊìæ
-
-
                 if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Platforms"))
                 {
                     PlaySound(ESoundEffect.HitStageObject);
-
-                    Debug.Log("è’ìÀÇµÇΩ: " + hit.collider.gameObject.name);
-
-                    //Instantiate(hitEffect, hit.point, Quaternion.identity);
-
+                    Debug.Log("Ë°ùÁ™Å„Åó„Åü: " + hit.collider.gameObject.name);
                     Explosion();
                 }
 
-                // è’ìÀÇµÇΩèÍçáÅAèeíeÇçÌèúÇ∑ÇÈ
                 Destroy(gameObject);
             }
         }
 
-        void Explosion()
+        private void Explosion()
         {
-            if(explosionPrefab!=null)
+            if (explosionPrefab != null)
             {
-                Instantiate(explosionPrefab);
+                if (effectService != null)
+                {
+                    effectService.PlayOneShotEffect(explosionPrefab, transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(explosionPrefab);
+                }
             }
-
         }
-
     }
-
-
 }
